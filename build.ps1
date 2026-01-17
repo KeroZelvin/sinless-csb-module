4a137b947d35f18e0754f61f1dd86a4f4bcee2d8
KeroZelvin <147951788+KeroZelvin@users.noreply.github.com>
2026-01-17 14:59:26 -0800

    add assets to build

--- build.ps1
+++ build.ps1
@@ -31,6 +31,7 @@ Copy-Item "scripts" (Join-Path $stage "scripts") -Recurse
 Copy-Item "packs"   (Join-Path $stage "packs")   -Recurse
 Copy-Item "csb"     (Join-Path $stage "csb")     -Recurse
 Copy-Item "styles"  (Join-Path $stage "styles")  -Recurse
+Copy-Item "assets"  (Join-Path $stage "assets")  -Recurse
 
 # Remove transient files
 Get-ChildItem -Path (Join-Path $stage "packs") -Recurse -Filter "LOCK" -ErrorAction SilentlyContinue |
